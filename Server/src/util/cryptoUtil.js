443dd586d3a14b0df840587c202fa8295c7da34e
dennydenny96 <dennydenny96@yahoo.com>
2021-09-05 19:16:40 +0700

    update

--- Server/src/util/cryptoUtil.js
+++ Server/src/util/cryptoUtil.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-
+import "babel-polyfill";
 const saltRounds = 10;
 const hash = async (text) => bcrypt.hash(text, saltRounds);
 const compare = async (text, hashed) => bcrypt.compare(text, hashed);
