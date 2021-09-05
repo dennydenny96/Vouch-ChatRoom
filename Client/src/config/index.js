e877d133e9a7b13e3b59ce3c38ae3ba72cf06a84
dennydenny96 <dennydenny96@yahoo.com>
2021-09-05 19:46:23 +0700

    Update index.js

--- Client/src/config/index.js
+++ Client/src/config/index.js
@@ -1,8 +1,13 @@
 const config = {
   server_node: {
-    url: 'http://localhost:4000',
+    // url: 'http://localhost:4000',
+    url: 'https://chat-room-app-server.herokuapp.com/',
     name: 'server_node',
   },
+  server_node_heroku: {
+    url: 'https://chat-room-app-server.herokuapp.com/',
+    name: 'server_node',
+  }
 };
 
 export default config;
